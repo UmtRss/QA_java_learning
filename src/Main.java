bacb8d8939b3fa2cfe4427311f1681fd2edbb822
UmtRss <umtrss@gmail.com>
2025-07-06 00:23:05 +0300

    Ders 7 Değişkenler eklendi

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int sayi = 10;
+        double pi = 3.14;
+        char harf = 'A';
+        boolean dogruMu = true;
+        String mesaj = "Hello QA World!";
+
+        System.out.println("Sayı: " + sayi);
+        System.out.println("Pi: " + pi);
+        System.out.println("Harf: " + harf);
+        System.out.println("Doğru mu?: " + dogruMu);
+        System.out.println("Mesaj: " + mesaj);
+    }
+}
