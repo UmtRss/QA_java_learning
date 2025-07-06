f01b94c2fc32094dd51303c364838ff8077803df
UmtRss <umtrss@gmail.com>
2025-07-06 18:35:34 +0300

    DeDers 12 Diziler eklendi

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,22 @@
 public class Main {
-    public static void main ( String[] args) {
-        //FOR LOOP
-        for (int i = 1; i<= 5; i++) {
-            System.out.println ("FOR LOOP REPEAT: " + i);
+    public static void main(String[] args) {
+       int [] sayilar = {10, 20, 30, 40, 50};
 
+       //Navigate Array with FOR
+        for ( int i = 0; i < sayilar.length; i++) {
+            System.out.println("FOR Element: " + sayilar [i]);
         }
-        //WHILE LOOP
-        int k = 1;
-        while (k <= 5) {
-            System.out.println("WHILE LOOP REPEAT: " + k);
-            k++;
+        // Navigate Array with WHILE
+        int j = 0;
+        while (j < sayilar.length) {
+            System.out.println("WHILE Element: " + sayilar[j]);
+            j++;
         }
-        // DO-WHILE LOOP
-        int m = 1;
+        // Navigate Array with DO-WHILE
+        int k = 0;
         do {
-            System.out.println(" DO-WHILE LOOP REPEAT: " + m);
-            m++;
-        } while ( m <= 5);
-
+            System.out.println("DO-WHILE Element: " + sayilar[k]);
+            k++;
+        } while (k < sayilar.length);
     }
-
 }
