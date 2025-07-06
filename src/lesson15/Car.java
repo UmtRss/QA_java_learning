5b662ee95fba1ec62b514bd356412da7a70ef3bb
UmtRss <umtrss@gmail.com>
2025-07-06 19:35:16 +0300

    Ders bazlı klasör yapısı kuruldu

---
+++ src/lesson15/Car.java
@@ -0,0 +1,12 @@
+package lesson15;
+
+public class Car {
+    String brand;
+    int modelYear;
+
+    public void Showinfo() {
+        System.out.println("Brand: " + brand);
+        System.out.println(" Model Year: " + modelYear);
+
+    }
+}
---
+++ src/lesson15/Main.java
@@ -0,0 +1,16 @@
+package lesson15;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car1 = new Car();
+        car1.brand = "Toyota";
+        car1.modelYear = 2020;
+        car1.Showinfo();
+
+        Car car2 = new Car();
+        car2.brand = "Honda";
+        car2.modelYear = 2018;
+        car2.Showinfo();
+
+    }
+}
